4a3928fbf73589bbfc26b40950aad0a02385110b
Mohammed Hameed <mohammed.hameed@email.com>
2018-04-24 20:41:37 -0500

    built table data, some routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,13 @@
 let path = require("path")
+let fs = require("fs")
 module.exports = function (app) {
+    let reservation;
+    fs.readFile(path.join(__dirname + '../data/reservation.json'), 'utf8', function (err, data) {
+        if (err) throw err;
+        reservation = JSON.parse(data)
+      });
+
+
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname , '../public/home.html'));
     })
@@ -11,4 +19,9 @@ module.exports = function (app) {
     app.get('/table', function (req, res) {
         res.sendFile(path.join(__dirname + '../public/table.html'));
     })
+
+    app.post('/table', function (req, res) {
+        res.send(reservation);
+    })
+
 }
